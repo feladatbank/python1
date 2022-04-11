e183ee36c389b308f884ea20e9d27d943694f5bf
Balázs <71691881+hadaszibalazs205@users.noreply.github.com>
2022-04-11 19:04:49 +0200

    Update p1.kutya.py

--- p1.kutya.py
+++ p1.kutya.py
@@ -1,8 +1,6 @@
 '''
 Írj egy programot, amely a felhasználótól bekéri egy kutya nevét, életkorát, fajtáját, és azt hogy rendelkezik-e érvényes oltással veszettség ellen! 
 Az adatokat tárolja a program szótárban, és írja ki a képernyőre az adatszerkezetet!
-
-lista = []
 '''
 nev = input('kutya neve: ')
 faj = input('kutya fajtája: ')
