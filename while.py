c47a8571b770d0ebdb5d023dfc07722695f76c78
Balázs <71691881+hadaszibalazs205@users.noreply.github.com>
2022-04-12 18:12:53 +0200

    Update while.py

--- while.py
+++ while.py
@@ -1,9 +1,16 @@
 '''
-1. Feladat
-Írj egy programot while ciklussal amely kiírja a páros számokat 1 és 10 között!
+Írj egy programot while vagy for ciklussal amely kiírja a páros számokat 1 és 10 között!
 '''
+
+#while
 
 szam = 0
 while szam < 10:
   szam = szam + 2
   print(szam)
+
+#for
+
+for i in range(11):
+    if i %2 == 0:
+        print(i)
