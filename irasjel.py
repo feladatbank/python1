e3b36d145876bec5fc258df306a785ceb83733b6
czompaldavid <95633227+czompaldavid@users.noreply.github.com>
2022-05-10 15:06:58 +0200

    Create irasjel.py

---
+++ irasjel.py
@@ -0,0 +1,10 @@
+#Készíts egy programot, amely a felhasználó által megadott mondatról a mondatvégi jel alapján eldönti milyen fajtájú! (kijelentő, kérdő, felkiáltó / felszólító / óhatjtó)
+#Lehetséges megoldás
+
+mondat = input("Írj le egy mondatot mondatvégi írásjellel ")
+if mondat[-1] =="!":
+  print("Ez egy felkiáltó / felszólító / óhajtó mondat ")
+if mondat[-1] == ".":
+  print("Ez egy kijelentő mondat ")
+if mondat[-1] == "?":
+  print("Ez egy kérdő mondat ")
